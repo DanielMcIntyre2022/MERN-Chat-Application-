f69f28ffa18378c948fcd4308c6c455b9abf7859
Daniel <daniel-mcintyre@hotmail.com>
2022-08-11 15:20:34 -0400

    routes setup

--- mern-chat-frontend/src/App.js
+++ mern-chat-frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './Components/Navigation';
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Chat from "./Pages/Chat";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
         < Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
